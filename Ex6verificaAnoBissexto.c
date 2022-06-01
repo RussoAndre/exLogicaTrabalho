e3ae2583cc166745000712407b47413bd9867637
André Pereira Russo <51973239+RussoAndre@users.noreply.github.com>
2022-06-01 11:31:56 -0300

    .

--- Ex6verificaAnoBissexto.c
+++ Ex6verificaAnoBissexto.c
@@ -5,7 +5,7 @@ int main(void) {
   
   printf("Informe o ano para verificarmos se é bissexto: \n");
   scanf("%d", &ano);
-  if((ano %400 ==0) || (ano %4 ==0) && (ano %100 ==0)){
+  if (((ano % 4 == 0) && (ano % 100 != 0)) || (ano % 400 == 0)){
     printf("O ano %d é bissexto", ano);
   } else{
      printf("O ano %d não é bissexto", ano);
